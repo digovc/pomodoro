6154a156e5af95a9c6de758ecb4159b96cd0717f
Rodrigo Daros <rodrigo@relatar.com.br>
2017-11-30 17:06:58 -0200

    Pomodoro:
    - Commit inicial.

---
+++ FrmPrincipal.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Pomodoro
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            this.nti = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cms = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmPomodoro = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmIntervaloCurto = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmIntervaloLongo = new System.Windows.Forms.ToolStripMenuItem();
+            this.cms.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nti
+            // 
+            this.nti.BalloonTipTitle = "Pomodoro";
+            this.nti.ContextMenuStrip = this.cms;
+            this.nti.Icon = ((System.Drawing.Icon)(resources.GetObject("nti.Icon")));
+            this.nti.Text = "Pomodoro";
+            this.nti.Visible = true;
+            // 
+            // cms
+            // 
+            this.cms.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmPomodoro,
+            this.tsmIntervaloCurto,
+            this.tsmIntervaloLongo});
+            this.cms.Name = "cms";
+            this.cms.Size = new System.Drawing.Size(155, 70);
+            // 
+            // tsmPomodoro
+            // 
+            this.tsmPomodoro.Name = "tsmPomodoro";
+            this.tsmPomodoro.Size = new System.Drawing.Size(154, 22);
+            this.tsmPomodoro.Text = "Pomodoro";
+            this.tsmPomodoro.Click += new System.EventHandler(this.tsmPomodoro_Click);
+            // 
+            // tsmIntervaloCurto
+            // 
+            this.tsmIntervaloCurto.Name = "tsmIntervaloCurto";
+            this.tsmIntervaloCurto.Size = new System.Drawing.Size(154, 22);
+            this.tsmIntervaloCurto.Text = "Intervalo curto";
+            // 
+            // tsmIntervaloLongo
+            // 
+            this.tsmIntervaloLongo.Name = "tsmIntervaloLongo";
+            this.tsmIntervaloLongo.Size = new System.Drawing.Size(154, 22);
+            this.tsmIntervaloLongo.Text = "Intervalo longo";
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(0, 0);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmPrincipal";
+            this.ShowInTaskbar = false;
+            this.Text = "Pomodoro";
+            this.cms.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon nti;
+        private System.Windows.Forms.ContextMenuStrip cms;
+        private System.Windows.Forms.ToolStripMenuItem tsmPomodoro;
+        private System.Windows.Forms.ToolStripMenuItem tsmIntervaloCurto;
+        private System.Windows.Forms.ToolStripMenuItem tsmIntervaloLongo;
+    }
+}
---
+++ FrmPrincipal.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Pomodoro
+{
+    public partial class FrmPrincipal : Form
+    {
+        #region Constantes
+
+        private enum EnmTipo
+        {
+            INTERVALO_CURTO,
+            INTERVALO_LONGO,
+            POMODORO,
+        }
+
+        #endregion Constantes
+
+        #region Atributos
+
+        private DateTime _dttInicio;
+
+        private DateTime dttInicio
+        {
+            get
+            {
+                return _dttInicio;
+            }
+
+            set
+            {
+                _dttInicio = value;
+            }
+        }
+
+        #endregion Atributos
+
+        #region Construtores
+
+        private FrmPrincipal()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion Construtores
+
+        #region Métodos
+
+        private void iniciarIntervalo(EnmTipo enmTipo)
+        {
+            this.dttInicio = DateTime.Now;
+
+            this.nti.Icon = Properties
+        }
+
+        #endregion Métodos
+
+        #region Eventos
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            this.nti.Visible = false;
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+
+            this.nti.Visible = true;
+            this.Visible = false;
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new FrmPrincipal());
+        }
+
+        private void tsmPomodoro_Click(object sender, EventArgs e)
+        {
+            this.iniciarIntervalo(EnmTipo.POMODORO);
+        }
+
+        #endregion Eventos
+    }
+}
